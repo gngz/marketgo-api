7f08c068a2fe4a9bec8b1379acdd95e60b848847
Gonçalo Passos <dev@diogopassos.pt>
2020-04-30 16:16:34 +0100

    Added list remove and update routines

--- app/Controllers/Http/ListController.js
+++ app/Controllers/Http/ListController.js
@@ -31,6 +31,40 @@ class ListController {
 
 
     }
+
+    // TODO better validation
+    async remove({ params, auth, request }) {
+        if (params.id) {
+            const user = auth.user;
+
+            const list = await List.find(params.id);
+
+            if (list && list.user_id == user.id) {
+                return list.delete();
+            }
+
+
+        }
+
+        return false;
+    }
+
+    // TODO better validation
+    async update({ params, auth, request }) {
+        if (params.id) {
+            const user = auth.user;
+            const list = await List.find(params.id);
+            const data = request.only(['name'])
+
+            if (list && list.user_id == user.id) {
+                list.merge(data)
+                await list.save()
+
+                return list;
+
+            }
+        }
+    }
 }
 
 module.exports = ListController
