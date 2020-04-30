d1d0587495bfe3a650f90066795d8e7cd56d24ba
Gonçalo Passos <dev@diogopassos.pt>
2020-04-30 14:41:40 +0100

    Created List Model and Migration

---
+++ database/migrations/1588253993000_list_schema.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ListSchema extends Schema {
+  up() {
+    this.create('lists', (table) => {
+      table.increments()
+      table.timestamps()
+      table.string("name")
+    })
+  }
+
+  down() {
+    this.drop('lists')
+  }
+}
+
+module.exports = ListSchema
