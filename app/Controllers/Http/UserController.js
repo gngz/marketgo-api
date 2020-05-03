c2b70e2506c2c7a1b9f83c32009fc3c5e757afd2
Manel Pereira <afonso.pereira4525@gmail.com>
2020-05-03 03:09:45 +0100

    Added facebook and google login through the frontend

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -16,7 +16,7 @@ class UserController {
             email: 'required|email|unique:users,email',
             password: 'required'
         }
-        const data = request.only(['email', 'firstName', 'lastName', 'password'])
+        const data = request.only(['email', 'name', 'password'])
         const validation = await validate(data, rules)
 
         if (validation.fails()) {
@@ -25,6 +25,47 @@ class UserController {
         const user = await User.create(data)
         const token = await auth.generate(user)
         return { "message": "registration successful", "token": token.token }
+    }
+
+    async googleLogin({ ally, auth, request }) {
+        if (request.input('token') == null) return;
+        var googleUser = await ally.driver('google').getUserByToken(request.input('token'))
+
+        const userDetails = {
+            email: googleUser.getEmail(),
+            avatar: googleUser.getAvatar(),
+            name: googleUser.getName(),
+        }
+
+        // search for existing user
+        const whereClause = {
+            email: googleUser.getEmail()
+        }
+
+        const user = await User.findOrCreate(whereClause, userDetails)
+        const token = await auth.generate(user)
+        return { ok: "yes" }
+    }
+
+    async facebookLogin({ ally, auth, request }) {
+        if (request.input('token') == null) return;
+        var facebookUser = await ally.driver('facebook').getUserByToken(request.input('token'))
+
+        const userDetails = {
+            email: facebookUser.getEmail(),
+            avatar: facebookUser.getAvatar(),
+            name: facebookUser.getName(),
+        }
+
+        // search for existing user
+        const whereClause = {
+            email: facebookUser.getEmail()
+        }
+
+        const user = await User.findOrCreate(whereClause, userDetails)
+        const token = await auth.generate(user)
+        return { ok: "yes" }
+
     }
 
     show({ auth, params }) {
