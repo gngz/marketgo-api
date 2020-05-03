ca4eb28583127e62d566fffcefd5f8843f7ae670
Manel Pereira <afonso.pereira4525@gmail.com>
2020-05-03 03:08:14 +0100

    Changed the table, there are no firstName and lastName now

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,11 +7,10 @@ class UserSchema extends Schema {
   up() {
     this.create('users', (table) => {
       table.increments()
-      table.string('firstName', 80).notNullable()
-      table.string('lastName', 80).notNullable()
+      table.string('name', 80).notNullable()
       table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.string('avatar', 60).nullable()
+      table.string('password', 60).nullable()
+      table.string('avatar', 255).nullable()
       table.timestamps()
     })
   }
