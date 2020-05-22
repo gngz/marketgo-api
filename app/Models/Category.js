7eb9d40be1dc11a4b32587569954e676337cd981
Gonçalo Passos <dev@diogopassos.pt>
2020-05-22 15:52:45 +0100

    Added hasMany relationship in Category Model

--- app/Models/Category.js
+++ app/Models/Category.js
@@ -11,6 +11,10 @@ class Category extends Model {
     father() {
         return this.belongsTo('App/Models/Category')
     }
+
+    products() {
+        return this.hasMany('App/Models/Product')
+    }
 }
 
 module.exports = Category
