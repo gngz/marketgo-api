07955d48b67bca7f197685339cfaa752284961d3
Manel Pereira <afonso.pereira4525@gmail.com>
2020-05-26 16:16:50 +0100

    Added local images to categories

--- database/seeds/CategorySeeder.js
+++ database/seeds/CategorySeeder.js
@@ -17,7 +17,7 @@ const Category = use('App/Models/Category')
 class CategorySeeder {
   async run() {
 
-    const Mercearia = await Category.create({ name: "Mercearia", image: "https://lh3.googleusercontent.com/proxy/C3f0gaRoiF-PGIMaWWZiDvLDB5-7fn2nucmLBLfB314pyS9uBvsA3Q6KJ6Y1XNbooB6i1MKG047f1_Fyas-tUSw1SidSgUsFXASB0AzrT29V2ByLhQhcQXYJrusLSkJreTRiy4OF39zbupz1IdNNL563uawdvUqtk9jO" });
+    const Mercearia = await Category.create({ name: "Mercearia", image: "assets/categories/groceries.png" });
     /*   (await Category.create({ name: "Arroz, Massa e Farinha" })).father().associate(Mercearia);
       (await Category.create({ name: "Azeite, Óleo e Vinagre" })).father().associate(Mercearia);
       (await Category.create({ name: "Molhos, Temperos e Sal" })).father().associate(Mercearia);
@@ -28,7 +28,7 @@ class CategorySeeder {
       (await Category.create({ name: "Doçaria e Chocolate" })).father().associate(Mercearia);
       (await Category.create({ name: "Sobremesas" })).father().associate(Mercearia); */
 
-    const Bebidas = await Category.create({ name: "Bebidas", image: "https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/github-mirkobrombin-bottles-icon.png" });
+    const Bebidas = await Category.create({ name: "Bebidas", image: "assets/categories/drinks.png" });
     /*     (await Category.create({ name: "Água" })).father().associate(Bebidas);
         (await Category.create({ name: "Sumos e Néctares" })).father().associate(Bebidas);
         (await Category.create({ name: "Refrigerantes" })).father().associate(Bebidas);
@@ -37,7 +37,7 @@ class CategorySeeder {
         (await Category.create({ name: "Espumantes" })).father().associate(Bebidas);
         (await Category.create({ name: "Chás, Cafés e Achoclatados" })).father().associate(Bebidas); */
 
-    const Lacticinios = await Category.create({ name: "Lacticínios", image: "https://cdn.iconscout.com/icon/free/png-256/cheese-1806482-1534540.png" });
+    const Lacticinios = await Category.create({ name: "Lacticínios", image: "assets/categories/dairy.png" });
     /*     (await Category.create({ name: "Iogurtes" })).father().associate(Lacticinios);
         (await Category.create({ name: "Leite" })).father().associate(Lacticinios);
         (await Category.create({ name: "Manetigas e Cremes Vegetais" })).father().associate(Lacticinios);
@@ -45,7 +45,7 @@ class CategorySeeder {
         (await Category.create({ name: "Queijos" })).father().associate(Lacticinios);
      */
 
-    const Limpeza = await Category.create({ name: "Limpeza", image: "https://icons.iconarchive.com/icons/robinweatherall/cleaning/256/bucket-icon.png" });
+    const Limpeza = await Category.create({ name: "Limpeza", image: "assets/categories/cleaning.png" });
     /*     (await Category.create({ name: "Limpeza Geral" })).father().associate(Limpeza);
         (await Category.create({ name: "Limpeza WC" })).father().associate(Limpeza);
         (await Category.create({ name: "Limpeza Cozinha" })).father().associate(Limpeza);
@@ -53,7 +53,7 @@ class CategorySeeder {
         (await Category.create({ name: "Ambientadores e Insecticidas" })).father().associate(Limpeza);
         (await Category.create({ name: "Sacos do Lixo" })).father().associate(Limpeza); */
 
-    const Animais = await Category.create({ name: "Animais", image: "https://lh3.googleusercontent.com/proxy/itZ8xMlAZncBhBiqbLmXzNuQ_7xisUHfq8ghGTw-0sbMuMzIz4xg_AIpGsP-YgzfQTUYjjVuE1D8V32FtpG2UKNy_EV8iTzjh5QeKX3b5z00PXGEyzc" });
+    const Animais = await Category.create({ name: "Animais", image: "assets/categories/pets.png" });
     /*     (await Category.create({ name: "Cão" })).father().associate(Animais);
         (await Category.create({ name: "Gato" })).father().associate(Animais);
         (await Category.create({ name: "Outros Animais" })).father().associate(Animais); */
